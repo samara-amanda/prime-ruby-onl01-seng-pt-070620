6ae41d22bf87a09a404348b04e9ed73220e6c634
Samara Grant <samara.amanda01@gmail.com>
2020-07-16 19:58:42 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,8 @@
 
 def prime?(num)
 
-  (2..(num - 1)).each do {|n| return false if num % n == 0}
-    return true
-  end
-end
+  (2..(num - 1)).each do |n|
+     return false if num % n == 0
+   end
+   return true
+ end
