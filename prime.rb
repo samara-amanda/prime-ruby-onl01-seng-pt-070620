bbb4196f220fc13fddc32f9f95abb5261fd0cf81
Samara Grant <samara.amanda01@gmail.com>
2020-07-16 19:55:03 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,16 +1,7 @@
 
 def prime?(num)
-  n = 2
+  if num < 2
+    return false
+  else
 
-  while n < num[n]
-    if num % n == 0
-      return false
-    end
-    n = n + 1
-  end
-  return true
-end
-
-
-def prime?(num)
-  (2..(num - 1)).each do {|n| num % n == 0}
+  (2..(num - 1)).each do {|n| return false if num % n == 0}
