70dfe7610960d4ca9b35ebd06774500af00e03ca
Samara Grant <samara.amanda01@gmail.com>
2020-07-16 20:08:08 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+
+def prime?(num)
+  if num >= 2
+    (2..(num - 1)).each do |n|
+      return false if num % n == 0
+    end
+    return true
+  else
+    return false
+  end
+end
