10ff295835dd4b66bf242eb3b1696d31ece2655f
Samara Grant <samara.amanda01@gmail.com>
2020-07-16 19:44:44 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,14 @@
 
 def prime?(num)
   n = 2
-  if num > -1
-    while n < num[n]
+  if num < 1
+    return false
+  end
+  while n < num[n]
     if num % n == 0
       return false
     end
     n = n + 1
-    end
-    return true
   end
+  return true
 end
