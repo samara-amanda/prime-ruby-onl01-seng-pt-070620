3d51e416bb2cb14586f3c0df6ca077528418795f
Samara Grant <samara.amanda01@gmail.com>
2020-07-16 18:58:19 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,10 @@
 # Add  code here!
 def prime?(num)
-  i = 0
-  while i < num[i]
+  n = 0
+  while i < num[n]
     if num % n == 0
       return false
-      i += 1
+      n += 1
     end
   end
   return true
